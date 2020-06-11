ed4920f93ce092888dd71defd6d476dec908780c
Ori <ori.azani@gmail.com>
2020-06-11 19:50:59 +0300

    second

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -4,48 +4,45 @@ var gElCanvas;
 var gCtx;
 var gCurrImg;
 
-
 function init() {
     gElCanvas = document.getElementById('my-canvas');
     gCtx = gElCanvas.getContext('2d');
-    renderGallery()
-  resizeCanvas();
+    renderGallery();
+    resizeCanvas();
 }
 
 function renderGallery() {
     var imgs = getImgs()
     var strHtmls = imgs.map(img => {
         return `<img onclick="onDrawImg(this)" src="${img.url}" alt="${img.keywords}"> `
-    })
+    });
     document.querySelector('.gallery-container').innerHTML = strHtmls.join('')
 }
-
-function renderMeme(){
-// clearCanvas()
-  onDrawImg(gCurrImg)
-  var meme = getMeme()
-  //console.log('meme' , meme)
-  meme.lines.forEach(line => drawText(line));
+    
+function renderMeme() {
+   // clearCanvas()
+    onDrawImg(gCurrImg)
+    var meme = getMeme()
+    //console.log('meme' , meme)
+    meme.lines.forEach(line => drawText(line));
 }
 
-
-function onDrawImg(img){
-gCurrImg = img
-gCtx.drawImage(img,0,0, gElCanvas.width, gElCanvas.width )
+function onDrawImg(img) {
+    gCurrImg = img
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.width)
 }
 
-function onUpdateText(elInput){
+function onUpdateText(elInput) {
     updateText(elInput.value)
     renderMeme()
 }
 
-function onUpdateSlectedLine(elInput){
+function onUpdateSlectedLine(elInput) {
     updateSelectedLine(elInput.id)
 }
 
-
 function drawText(lineObj) {
- //  console.log(lineObj)
+    //  console.log(lineObj)
     gCtx.lineWidth = '5';
     gCtx.font = `${lineObj.size}px impact`;
     gCtx.textAlign = lineObj.align;
@@ -55,35 +52,69 @@ function drawText(lineObj) {
     gCtx.fillText(lineObj.txt, lineObj.x, lineObj.y);
 }
 
-function onChangeFontSize(diff){
+function onChangeFontSize(diff) {
     changeFontSize(diff)
     renderMeme()
 }
 
-function onChangeTextPosition(diff){
-       changeTextPosition(diff)
-       renderMeme()
+function onChangeTextPosition(diff) {
+    changeTextPosition(diff)
+    renderMeme()
 }
 
-function onSwitchLine(){
+function onSwitchLine() {
     // console.log('switch line in')
     switchLine()
-    var meme  = getMeme()
+    var meme = getMeme()
     focusOnLine(meme.selectedLineIdx)
     renderMeme()
 }
 
-function focusOnLine(lineIdx){
-var elInput = document.getElementById(lineIdx)
-elInput.focus();
+function focusOnLine(lineIdx) {
+    var elInput = document.getElementById(lineIdx)
+    elInput.focus();
 }
 
+function show(li) {
+    var liClass = li.className
+console.log(liClass)
+    switch (liClass) {
+        case "gallery-li":
+            var gallery = document.querySelector('.gallery-container');
+            gallery.classList.remove('hide')
+            var center = document.querySelector('.center');
+            center.classList.add('hide')
+            break;
 
-// function clearCanvas() {
-//     gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
-// }
+        case "meme-li":
+            var gallery = document.querySelector('.gallery-container');
+            gallery.classList.add('hide')
+            var center = document.querySelector('.center');
+            center.classList.remove('hide')
+            break;
+        
+        case "class3":
+            test.innerHTML = "I have class3";
+            break;
+        case "class4":
+            test.innerHTML = "I have class4";
+            break;
+        default:
+            test.innerHTML = "";
+    }
 
-function resizeCanvas(){
+}
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function onDownloadCanvas(elLink) {
+    const data = gElCanvas.toDataURL();
+    elLink.href = data;
+    elLink.download = 'my_img';
+}
+
+function resizeCanvas() {
     var elContainer = document.querySelector('.canvas-container');
     gElCanvas.width = elContainer.offsetWidth;
     gElCanvas.height = elContainer.offsetHeight;
--- js/meme-service.js
+++ js/meme-service.js
@@ -6,6 +6,21 @@ var gImgs = [
     { id: 1, url: 'img/1.jpg', keywords: ['happy'] },
     { id: 2, url: 'img/2.jpg', keywords: ['happy'] },
     { id: 3, url: 'img/3.jpg', keywords: ['happy'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['happy'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['happy'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['happy'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['happy'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['happy'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['happy'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['happy'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['happy'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['happy'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['happy'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['happy'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['happy'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['happy'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['happy'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['happy'] },
 ];
 
 var gMeme = {
@@ -26,7 +41,7 @@ var gMeme = {
             align: 'left',
             color: 'white',
             x:50,
-            y:400,
+            y:250,
         }
     ]
 }
@@ -40,9 +55,8 @@ function getMeme() {
 }
 
 function updateText(text){  //changing the text on gMemes >> lines >>txt
-    //console.log('dfdfdfdfdf')
+    //console.log('in updatetext ')
     gMeme.lines[gMeme.selectedLineIdx].txt = text;
-    
 }
 
 function changeFontSize(diff){
@@ -57,7 +71,7 @@ function changeTextPosition(diff){
 function switchLine(){
    if (gMeme.selectedLineIdx === 0) gMeme.selectedLineIdx = 1
    else gMeme.selectedLineIdx = 0
-   console.log(gMeme.selectedLineIdx)
+//    console.log(gMeme.selectedLineIdx)
 
 }
 function updateSelectedLine(inputId){
